a7302b80d1819d92f575aa4c0920bd394c9fb203
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2025-04-30 14:36:01 +0200

    Create 11-need_meeting.sql

---
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -0,0 +1,8 @@
+-- 11. No table for a meeting
+-- Create a view for students who need a meeting
+
+CREATE VIEW need_meeting AS
+SELECT name
+FROM students
+WHERE score < 80
+  AND (last_meeting IS NULL OR last_meeting < CURDATE() - INTERVAL 1 MONTH);
