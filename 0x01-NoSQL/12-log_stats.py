e971237f94c68f293d0e9ec25442453be96b5e39
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2025-05-05 21:57:42 +0200

    Create 12-log_stats.py

---
+++ 0x01-NoSQL/12-log_stats.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""Log stats - basic"""
+
+
+from pymongo import MongoClient
+
+if __name__ == "__main__":
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    collection = client.logs.nginx
+
+    total_logs = collection.count_documents({})
+    print(f"{total_logs} logs")
+
+    print("Methods:")
+    for method in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
+        count = collection.count_documents({"method": method})
+        print(f"\tmethod {method}: {count}")
+
+    status_count = collection.count_documents({"method": "GET", "path": "/status"})
+    print(f"{status_count} status check")
